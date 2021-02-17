2b921c6d655dc3464cede81a44bbcfabf71350bb
David Canagasabey <david@dtcan.dev>
2021-02-17 12:16:44 -0500

    Pull back camera

--- js/index.js
+++ js/index.js
@@ -14,7 +14,7 @@ const boids = [];
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 50;
+camera.position.z = 80;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
